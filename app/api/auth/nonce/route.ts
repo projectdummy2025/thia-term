98c1113bd02ffe63363daddc65883f5f7036e25e
maulana-tech <firdaussyah03@gmail.com>
2026-06-21 10:13:16 +0700

    delete(app/api/auth/nonce/route.ts): auto update

--- app/api/auth/nonce/route.ts
+++
@@ -1,9 +0,0 @@
-export const dynamic = 'force-dynamic'
-
-import { NextResponse } from "next/server"
-import { generateNonce } from "siwe"
-
-export async function GET() {
-  const nonce = generateNonce()
-  return NextResponse.json({ nonce })
-}
